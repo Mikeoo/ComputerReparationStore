750114a92550ea61b4cfc485ef43df38533ddbb2
Mike Overbeek <38927348+Mikeoo@users.noreply.github.com>
2020-07-06 12:01:47 +0200

    Created models

---
+++ ComputerReparationStore/Models/Part.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerReparationStore.Models
+{
+    public class Part : ListAllParts
+    {
+        public bool InStock { get; set; }
+    }
+}
---
+++ ComputerReparationStore/Models/ReparationOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerReparationStore.Models
+{
+    public class ReparationOrder
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        Awaiting,
+        Processing,
+        AwaitingParts,
+        ReparationDone,
+        WaitingForPickup,
+        Closed
+    }
+}
