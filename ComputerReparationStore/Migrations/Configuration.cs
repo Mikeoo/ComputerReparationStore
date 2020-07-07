c920473bba81f78fe9d67a9b90686290f046617d
Mike Overbeek <mikeoverbeek@outlook.com>
2020-07-07 13:11:30 +0200

    seeddata

--- ComputerReparationStore/Migrations/Configuration.cs
+++ ComputerReparationStore/Migrations/Configuration.cs
@@ -1,6 +1,5 @@
 namespace ComputerReparationStore.Migrations
 {
-    using ComputerReparationStore.DAL;
     using ComputerReparationStore.Models;
     using System.Data.Entity.Migrations;
 
@@ -20,17 +19,25 @@ namespace ComputerReparationStore.Migrations
             context.Customers.AddOrUpdate(x => x.Id,
                 new Models.Customer() { Id = 1, FirstName = "Jane" },
                 new Models.Customer() { Id = 2, FirstName = "Piet" },
-                new Models.Customer() { Id = 3, FirstName = "Jane" }
-        );
+                new Models.Customer() { Id = 3, FirstName = "Rowan" });
+
             context.ReparationOrders.AddOrUpdate(x => x.Id,
                 new ReparationOrder()
                 {
+                    Id = 1,
+                    StartDate = new System.DateTime(2020, 07, 07),
+                    EndDate = new System.DateTime(2020, 07, 07),
+                    Status = Status.Awaiting
+                },
+                new ReparationOrder()
+                {
+                    Id = 2,
                     StartDate = new System.DateTime(2020, 07, 07),
                     EndDate = new System.DateTime(2020, 07, 07),
                     Status = Status.Awaiting
                 }
                 );
+
         }
-            
     }
 }
