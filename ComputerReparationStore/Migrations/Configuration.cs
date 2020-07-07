f94a0f20d4b0afe602049004c1db10630fcc920d
Mike Overbeek <mikeoverbeek@outlook.com>
2020-07-07 15:35:51 +0200

    customer seed

--- ComputerReparationStore/Migrations/Configuration.cs
+++ ComputerReparationStore/Migrations/Configuration.cs
@@ -2,6 +2,7 @@ namespace ComputerReparationStore.Migrations
 {
     using ComputerReparationStore.Models;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ComputerReparationStore.DAL.ReparationOrderContext>
     {
@@ -17,24 +18,25 @@ namespace ComputerReparationStore.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
             context.Customers.AddOrUpdate(x => x.Id,
-                new Models.Customer() { Id = 1, FirstName = "Jane" },
-                new Models.Customer() { Id = 2, FirstName = "Piet" },
-                new Models.Customer() { Id = 3, FirstName = "Rowan" });
+                new Models.Customer() { FirstName = "Jane" },
+                new Models.Customer() { FirstName = "Piet" },
+                new Models.Customer() { FirstName = "Rowan" });
 
             context.ReparationOrders.AddOrUpdate(x => x.Id,
                 new ReparationOrder()
-                {
-                    Id = 1,
+                {                  
                     StartDate = new System.DateTime(2020, 07, 07),
-                    EndDate = new System.DateTime(2020, 07, 07),
-                    Status = Status.Awaiting
+                    EndDate = new System.DateTime(2020, 07, 12),
+                    Status = Status.Awaiting,
+                    Customer = context.Customers.FirstOrDefault(c => c.FirstName == "Rowan")
                 },
                 new ReparationOrder()
                 {
                     Id = 2,
                     StartDate = new System.DateTime(2020, 07, 07),
-                    EndDate = new System.DateTime(2020, 07, 07),
-                    Status = Status.Awaiting
+                    EndDate = new System.DateTime(2020, 07, 10),
+                    Status = Status.Awaiting,
+                    Customer = context.Customers.FirstOrDefault(c => c.FirstName == "Jane")
                 }
                 );
 
